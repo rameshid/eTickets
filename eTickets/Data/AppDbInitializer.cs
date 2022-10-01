6b04c8abf3bb29d8b73dc0c6602de100675bfcb5
SSD <SSD@DESKTOP-8VIUELM>
2022-10-02 00:43:45 +0530

    Add project files.

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Cinema
+                if(context.Cinemas.Any())
+                {
+
+                }
+                //Actor
+                if(context.Actors.Any())
+                {
+
+                }
+                //Movie
+                if(context.Movies.Any())
+                {
+
+                }
+                //Producer
+                if(context.Producers.Any())
+                {
+
+                }
+                //Actor_Movie
+                if(context.Actor_Movies.Any())
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        // Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
